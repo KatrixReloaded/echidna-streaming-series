ea3632146c1ffcfb942c64653e0c97fbdc845b90
KatrixReloaded <kartikeyasureka@gmail.com>
2025-01-17 13:28:59 +0530

    goj made me do it

--- part1/exercise1/template.sol
+++ part1/exercise1/template.sol
@@ -1,12 +1,15 @@
-import "token.sol";
+pragma solidity ^0.7.0;
+import "./token.sol";
 
 contract TestToken is Token {
     address echidna_caller = msg.sender;
 
-    constructor() public {
+    constructor() {
         balances[echidna_caller] = 10000;
     }
 
     // add the property
-    function echidna_test_balance() public view returns (bool) {}
+    function echidna_test_balance() public view returns (bool) {
+        return (balances[echidna_caller] <= 10000);
+    }
 }
--- part1/exercise1/token.sol
+++ part1/exercise1/token.sol
@@ -37,7 +37,22 @@ contract Token is Pausable{
     mapping(address => uint) public balances;
 
     function transfer(address to, uint value) ifNotPaused public{
-        balances[msg.sender] -= value;
+        // since it's 0.7.0, no SafeMath, so if value is greater than balances[msg.sender], it will underflow
+        balances[msg.sender] -= value; 
         balances[to] += value;
+    }
+}
+
+// Fixed contract
+contract Token is Pausable{
+    mapping(address => uint) public balances;
+
+    function transfer(address to, uint value) ifNotPaused public{
+        uint256 preBalanceMsgSender = balances[msg.sender];
+        uint256 preBalanceTo = balances[to];
+        balances[msg.sender] -= value; 
+        balances[to] += value;
+        require(balances[msg.sender] <= preBalanceMsgSender);
+        require(balances[to] >= preBalanceTo);
     }
 }
